18135431dc3930dde13428ed24f7b4b9b41d0f54
ricardopimentel <ricardoppimentel@gmail.com>
2017-06-29 18:17:30 -0300

    Keynote speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -15,6 +15,26 @@ class HomeTest(TestCase):
         """Must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
 
+
     def test_sunscription_link(self):
         expected = 'href="{}"'.format(r('subscriptions:new'))
+        self.assertContains(self.response, expected)
+
+
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents = [
+            "Grace Hopper",
+            "http://hbn.link/hopper-pic",
+            "Alan Turing",
+            "http://hbn.link/turing-pic",
+        ]
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
